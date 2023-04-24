7039a1f5da93ca8f1d5133291e8a8f230b62d37e
marcusbroman <marcus.broman@skola.taby.se>
2023-04-24 09:40:37 +0200

    Version 0.2.4(Birds)

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ document.getElementById("menuBtn").addEventListener("click", openNav)
 document.getElementById("closeBtn").addEventListener("click", closeNav)
 
 function openNav() {
-    if(document.getElementById("mySidebar").style.width == "250px"){
+    if(document.getElementById("mySidebar").style.width == "22vw"){
       closeNav()
     }
     else{ 
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";}
+    document.getElementById("mySidebar").style.width = "22vw";
+    document.getElementById("menuBtn").style.marginLeft = "23vw"
+  }
 }
 
   function closeNav() {
+    document.getElementById("menuBtn").style.marginLeft = "1vw"
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
     return x=0
   }
 
